b74aadec48b4cee142383fdb2aacf6e229a175a3
Ayyaz <developer1.oliver@gmail.com>
2021-07-01 03:20:26 +0500

    back to Dashboard

--- lib/Dashboard.dart
+++ lib/Dashboard.dart
@@ -12,39 +12,55 @@ class Dashboard extends StatefulWidget {
 
 class _DashboardState extends State<Dashboard> {
   var _currentIndex = 0;
+
+  Future<bool> onWillPop() {
+    if (_currentIndex != 0) {
+      setState(() {
+        _currentIndex = 0;
+      });
+      return Future.value(false);
+    } else {
+      return Future.value(true);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // all of the main screens in the dashboard will use the same appbar, no need to code appbar in new pages
-        backgroundColor: Colors.red[900],
-        title: Text(_currentIndex == 0
-            ? 'Dashboard'
-            : _currentIndex == 1
-                ? 'Profile'
-                : 'Contact'),
-        elevation: 0,
+    return WillPopScope(
+      onWillPop: onWillPop,
+      child: Scaffold(
+        appBar: AppBar(
+          // all of the main screens in the dashboard will use the same appbar, no need to code appbar in new pages
+          backgroundColor: Colors.red[900],
+          title: Text(_currentIndex == 0
+              ? 'Dashboard'
+              : _currentIndex == 1
+                  ? 'Profile'
+                  : 'Contact'),
+          elevation: 0,
+        ),
+        backgroundColor: Colors.white,
+        bottomNavigationBar: BottomNavigationBar(
+          currentIndex: _currentIndex,
+          type: BottomNavigationBarType.fixed,
+          onTap: (index) {
+            setState(() {
+              index == 3 ? Get.dialog(LogoutDialog()) : _currentIndex = index;
+            });
+          },
+          items: [
+            BottomNavigationBarItem(
+                icon: Icon(Icons.home), title: Text("Home")),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.account_box), title: Text("Profile")),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.contact_page), title: Text("Contact")),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.logout), title: Text("Logout")),
+          ],
+        ),
+        body: getBodyWidget(),
       ),
-      backgroundColor: Colors.white,
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _currentIndex,
-        type: BottomNavigationBarType.fixed,
-        onTap: (index) {
-          setState(() {
-            index == 3 ? Get.dialog(LogoutDialog()) : _currentIndex = index;
-          });
-        },
-        items: [
-          BottomNavigationBarItem(icon: Icon(Icons.home), title: Text("Home")),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.account_box), title: Text("Profile")),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.contact_page), title: Text("Contact")),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.logout), title: Text("Logout")),
-        ],
-      ),
-      body: getBodyWidget(),
     );
   }
 
